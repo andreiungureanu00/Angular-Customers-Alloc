56d04baf1d179f1bfc2672a8c36f99c80ef44a5e
Andrei Ungureanu <andrei090498@gmail.com>
2021-04-26 14:02:16 +0300

    added graphql backend

--- backend/GraphQL/schema.js
+++ backend/GraphQL/schema.js
@@ -15,7 +15,7 @@ const schema = graphql.buildSchema(`
     project_code: String
   },
   type Mutation {
-		updateProject(id: Int, project_name: String, start_date: DateTime,
+    updateProject(id: Int, project_name: String, start_date: DateTime,
       planned_end_date: DateTime, description: String, project_code:
       String): Project,
     deleteProject(id: Int): String,
